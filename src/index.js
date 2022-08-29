8357dbae3bf07951b7d550606a614d9d90fb69fc
Peter Marcano <pbmarcano91@gmail.com>
2022-08-28 20:20:26 -0400

    Try simpler approach

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<Splash />} />
         <Route path="/play" element={<App />} />
