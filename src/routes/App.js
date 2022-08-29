15c641a4cee2aa354592ed82cf1d4e900e50137f
Peter Marcano <pbmarcano91@gmail.com>
2022-08-28 21:51:17 -0400

    Add animations to prompts

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,18 +1,24 @@
-import { useState } from "react";
-import tipsyRules from "../rules/tipsy.json";
-import { Wrapper } from "../components/Wrapper";
-import { Sponsor } from "../components/Sponsor";
+import { useState } from "react"
+import tipsyRules from "../rules/tipsy.json"
+import { Wrapper } from "../components/Wrapper"
+import { Sponsor } from "../components/Sponsor"
+import { Transition } from "@headlessui/react"
 
 export function App() {
-  const [rule, setRule] = useState(randomRule());
+  const [rule, setRule] = useState(randomRule())
+  const [isShowing, setIsShowing] = useState(true)
 
   function randomRule() {
-    const index = Math.floor(Math.random() * tipsyRules.length);
-    return tipsyRules[index];
+    const index = Math.floor(Math.random() * tipsyRules.length)
+    return tipsyRules[index]
   }
 
   function next() {
-    setRule(randomRule());
+    setIsShowing(false)
+    setTimeout(() => {
+      setRule(randomRule())
+      setIsShowing(true)
+    }, 200)
   }
 
   return (
@@ -20,12 +26,32 @@ export function App() {
       <div className="text-white text-center grow flex flex-col">
         <Sponsor />
         <div className="m-auto">
-          <h2 className="text-4xl font-semibold">
-            {rule.name}
-          </h2>
-          <p className="text-lg mt-4 w-3/4 mx-auto font-thin">
-            {rule.description}
-          </p>
+          <Transition show={isShowing}>
+            <Transition.Child
+              enter="transition ease-in-out duration-75"
+              enterFrom="scale-75 opacity-0"
+              enterTo="scale-100 opacity-100"
+              leave="transition ease-in-out duration-75"
+              leaveFrom="scale-100 opacity-100"
+              leaveTo="scale-75 opacity-0"
+            >
+              <h2 className="text-4xl font-semibold">
+                {rule.name}
+              </h2>
+            </Transition.Child>
+            <Transition.Child
+              enter="transition ease-in-out duration-75"
+              enterFrom="translate-x-full opacity-0"
+              enterTo="translate-x-0 opacity-100"
+              leave="transition ease-in-out duration-75"
+              leaveFrom="translate-x-0 opacity-100"
+              leaveTo="-translate-x-full opacity-0"
+            >
+              <p className="text-lg mt-4 w-3/4 mx-auto font-thin">
+                {rule.description}
+              </p>
+            </Transition.Child>
+          </Transition>
         </div>
       </div>
       <div className="mb-24 text-center">
@@ -36,5 +62,5 @@ export function App() {
         </button>
       </div>
     </Wrapper>
-  );
+  )
 }
